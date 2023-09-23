06032e79d954ef697a625e9cbad697a6f86017e6
houssemkammoun <houssemkammoun99@gmail.com>
2023-09-23 15:56:02 +0100

    add features

--- src/test/java/Methods/APIRequestMethods.java
+++ src/test/java/Methods/APIRequestMethods.java
@@ -1,8 +1,11 @@
 package Methods;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import java.io.IOException;
 import static io.restassured.RestAssured.given;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 public class APIRequestMethods {
     private static String apiKey = "61f4248c9d9bb038eaf0c6c0";
@@ -18,4 +21,14 @@ public class APIRequestMethods {
     public static void verifyStatusCode(Response response, int expectedStatusCode) {
         response.then().log().all().statusCode(expectedStatusCode);
     }
+
+    public static void verifyNumberOfEmployees(Response response, int expectedCount) {
+        JsonPath jsonPath = response.jsonPath();
+        int actualCount = jsonPath.getList("data").size();
+        
+        Assert.assertEquals(actualCount, expectedCount, "Number of employees does not match the expected count.");
+    }
+
+
+
 }
